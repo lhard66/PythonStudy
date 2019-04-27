154f4901a6ee20d123f230eb41aa62708c719144
LiuTianqing <lhard@qq.com>
2019-04-27 15:59:40 +0800

    lambda

---
+++ relive/lambda.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 请用匿名函数改造下面的代码：
+def is_odd(n):
+  return n % 2 == 1
+
+L = list(filter(is_odd, range(1, 20)))
+L2 = list(filter(lambda n: n % 2 == 1, range(1, 20)))
+
+print(L)
+print(L2)
